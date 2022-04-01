2d9d275a5c6ea401076ea6fbb9c1ff6e894794ba
makerio90 <makerio1@pm.me>
2022-04-01 16:11:03 -0400

    workflows

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,38 @@
 use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let num: f64 = args[1].parse().unwrap();
+    let num: i32 = args[1].parse().unwrap();
     let perfect_square = find_perfect_sqare(num);
-    let multiple = (num / perfect_square).sqrt();
+    let multiple = num / perfect_square;
+    let multiple: f64 = f64::from(multiple).sqrt();
     println!("sqrt({}) * {} == sqrt({})", perfect_square, multiple, num)
 }
 #[test]
 fn is() {
-  assert!( is_perfect_square(4.0));
-  assert!(! is_perfect_square(2.0));
-  assert!(! is_perfect_square(8.0));
-  assert!( is_perfect_square(16.0));
+  assert!( is_perfect_square(4));
+  assert!(! is_perfect_square(2));
+  assert!(! is_perfect_square(8));
+  assert!( is_perfect_square(16));
 }
 #[test]
 fn find(){
-  assert_eq!(find_perfect_sqare(8.0), 2.0);
-  assert_eq!(find_perfect_sqare(132.0), 33.0);
-  assert_eq!(find_perfect_sqare(325.0), 13.0);
-  assert_eq!(find_perfect_sqare(136.0), 34.0);
+  assert_eq!(find_perfect_sqare(8), 2);
+  assert_eq!(find_perfect_sqare(132), 33);
+  assert_eq!(find_perfect_sqare(325), 13);
+  assert_eq!(find_perfect_sqare(136), 34);
 }
-fn is_perfect_square(n: f64) -> bool{
+fn is_perfect_square(n: i32) -> bool{
+    let n: f64 = f64::from(n);
     n > 0.0 && n.sqrt() % 1.0 == 0.0
 }
-fn find_perfect_sqare(num: f64) -> f64{
-  let mut divisor: f64 = 1.0;
+fn find_perfect_sqare(num: i32) -> i32{
+  let mut divisor: i32 = 1;
   loop {
-    divisor += 1.0;
-    if num % divisor == 0.0 &&  is_perfect_square(num / divisor){
+    divisor += 1;
+    if num % divisor == 0 &&  is_perfect_square(num / divisor){
 	    return divisor
 		}
-    if divisor > 100.0{
+    if divisor > 1000{
       panic!("infinite loop!")
     }
   }
